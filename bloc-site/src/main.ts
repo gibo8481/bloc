913f37a255697ebad0fcdcd129c1880bcbc8c911
Gia <gibo8481@colorado.edu>
2025-04-28 18:58:33 -0600

    Revert "AGH"
    This reverts commit c1b737f5ef22cd39615ed5c62e2df02cf939ea56.

--- bloc-site/src/main.ts
+++ bloc-site/src/main.ts
@@ -1,13 +1,17 @@
 import './style.css'
 
-import { loadNavbar } from "./components/navbar";
-import { loadFooter } from "./components/footer";
-import { setupAboutPage } from './pages/about';
-import { setupModelPage } from './pages/model';
-import { setupInnovationPage } from './pages/innovation';
+import { loadNavbar } from "./components/navbar.ts";
+import { loadFooter } from "./components/footer.ts";
+import { setupAboutPage } from './pages/about.ts';
+import { setupModelPage } from './pages/model.ts';
+import { setupInnovationPage } from './pages/innovation.ts';
+
+
 
 loadNavbar();
 loadFooter();
+
+setupModelPage();
 
 if (window.location.pathname.endsWith("model.html")) {
   setupModelPage();
@@ -16,7 +20,7 @@ if (window.location.pathname.endsWith("model.html")) {
 if (window.location.pathname.endsWith("about.html")) {
   setupAboutPage();
 }
-
 document.addEventListener('DOMContentLoaded', () => {
   setupInnovationPage();
 });
+
--- bloc-site/vite.config.ts
+++ bloc-site/vite.config.ts
@@ -1,49 +1,5 @@
 import { defineConfig } from 'vite';
-import { createHtmlPlugin } from 'vite-plugin-html';
 
 export default defineConfig({
-  base: '/',
-  plugins: [
-    createHtmlPlugin({
-      inject: {
-        tags: [
-          {
-            tag: 'link',
-            attrs: {
-              rel: 'stylesheet',
-              href: '/assets/index-DPXLfH-n.css', // Adjust this path as needed
-            },
-            injectTo: 'head',
-          },
-        ],
-      },
-    }),
-  ],
-  build: {
-    rollupOptions: {
-      input: {
-        main: './index.html',
-        about: './public/about.html',
-        bloc: './public/bloc.html',
-        footer: './public/footer.html',
-        innovationGuide: './public/innovation-guide.html',
-        keychain: './public/keychain.html',
-        nameplate: './public/nameplate.html',
-        navbar: './public/navbar.html',
-        ourJourney: './public/our-journey.html',
-        phoneStand: './public/phone-stand.html',
-        projects: './public/projects.html',
-        topo: './public/topo.html'
-      }
-    },
-    assetsDir: 'assets',
-    cssCodeSplit: false,
-    outDir: 'dist'
-  },
-  publicDir: 'public',
-  resolve: {
-    alias: {
-      '/src': './src'
-    }
-  }
+  base: '/', // using root path for custom domain blocnc.com
 });
